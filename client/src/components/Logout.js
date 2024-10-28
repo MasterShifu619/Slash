d8dd1af4c2c202594792cbdb90686dc5457d26df
Ashlesha Joshi <67588914+aj18coep@users.noreply.github.com>
2024-10-27 17:44:47 -0800

    Updated Logout.js

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,13 +1,33 @@
-// Logout.js
-import React from "react";
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const Logout = () => {
+function Logout() {
+  const navigate = useNavigate();
   return (
-    <div>
-      {/* You can customize the content of the logout page */}
-      <h2>User logged out successfully!</h2>
-    </div>
+    <Box 
+      display="flex" 
+      flexDirection="column" 
+      justifyContent="center" 
+      alignItems="center" 
+      height="100vh" 
+      bgcolor="#f5f5f5" 
+      textAlign="center"
+      sx={{ 
+        margin: 0, 
+        padding: 0, 
+        position: 'absolute', 
+        top: 0, 
+        left: 0, 
+        right: 0, 
+        bottom: 0 
+      }}
+    >
+      <Typography variant="h4" sx={{ color: '#333' }}>
+        You have successfully logged out!
+      </Typography>
+      
+    </Box>
   );
-};
-
+}
 export default Logout;
